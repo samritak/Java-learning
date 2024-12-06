608b6db441629ef810223528e29074841b03a839
samritak <152491485+samritak@users.noreply.github.com>
2024-12-06 13:09:39 +0530

    Update Hashmap.java

--- Hashmap.java
+++ Hashmap.java
@@ -1,31 +1,33 @@
+
+
 import java.util.HashMap;
 import java.util.Map;
-public class LibrarySystem {
- public static void main(String[] args) {
- Map<String, String> books = new HashMap<>();
- books.put("Effective Java", "Joshua Bloch");
- books.put("Java Concurrency in Practice", "Brian Goetz");
- books.put("Clean Code", "Robert C. Martin");
- System.out.println("Books in the library:");
+public class LibrabySystem {
+public static void main(String[] args) {
+Map<String , String> books = new HashMap<>();
+
+books.put("Book1","Author1");
+books.put("Book2","Author2");
+books.put("Book3","Author3");
+
+ System.out.println("Books in Library are :");
  for (Map.Entry<String, String> entry : books.entrySet()) {
- System.out.println("Title: " + entry.getKey() + ", Author: " + 
-entry.getValue());
+ System.out.println("Title is "+entry.getKey()+", Author is "+entry.getValue());
  }
 
-// Lookup an author by book title
- String title = "Effective Java";
- if (books.containsKey(title)) {
- System.out.println("The author of " + title + " is " + 
-books.get(title));
- } else {
- System.out.println(title + " is not in the library.");
+String title ="Book2";
+if (books.containsKey(title)){
+    System.out.println("\nFound the book: "+books.get(title));
 }
- // Remove a book by title
- books.remove("Java Concurrency in Practice");
- System.out.println("Books after removal:");
+else{
+    System.out.println("\nBook is not present");
+}
+
+books.remove("Book2");
+
+ System.out.println("After removed books in Library are :");
  for (Map.Entry<String, String> entry : books.entrySet()) {
- System.out.println("Title: " + entry.getKey() + ", Author: " + 
-entry.getValue());
+ System.out.println("Title is "+entry.getKey()+", Author is "+entry.getValue());
  }
  }
 }
